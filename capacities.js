f624a06110e90e348765fea1582eb2668b6d686b
utkarsh-1mg <utkarsh.srivastava@1mg.com>
2022-04-18 18:09:06 +0530

    remove builds

--- capacities.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.IntersectionObserver = void 0;
-var IntersectionObserver = typeof window !== 'undefined' && window.IntersectionObserver;
-exports.IntersectionObserver = IntersectionObserver;
